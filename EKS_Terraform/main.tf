dd23e1a95a4d2ddbe46d5e6adc4ae730114e32ef
harsha <harshavardhanrock007@gmail.com>
2024-11-29 11:41:56 +0530

    region changes

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "devopsshack_vpc" {
@@ -14,7 +14,7 @@ resource "aws_subnet" "devopsshack_subnet" {
   count = 2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["eu-west-2a", "eu-west-2c"], count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1c"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
